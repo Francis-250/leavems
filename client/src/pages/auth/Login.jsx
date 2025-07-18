3a41f23f8bf8f3792a21e167dfe51077bbf24125
Francis <munyankindif0@gmail.com>
2025-07-18 11:18:36 +0200

    changes 14

--- client/src/pages/auth/Login.jsx
+++ client/src/pages/auth/Login.jsx
@@ -3,7 +3,7 @@ import { SignIn } from "@clerk/clerk-react";
 const Login = () => {
   return (
     <div className="flex items-center justify-center h-screen bg-black">
-      <SignIn afterSignInUrl="/admin" redirectUrl="/admin" />
+      <SignIn afterSignInUrl="/" redirectUrl="/" />
     </div>
   );
 };
