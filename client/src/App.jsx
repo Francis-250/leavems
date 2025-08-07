795f6a20ecd4b2f8973112c5f22e62e7505ab51c
Munyankindi François <147691671+Francis-250@users.noreply.github.com>
2025-08-07 16:10:47 +0200

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -42,7 +42,7 @@ const App = () => {
           }
         /> */}
         <Route
-          path="/admin"
+          path="/admin/leave"
           element={
             isSignedIn ? <LeaveAdmin /> : <Navigate to="/login" replace />
           }
